4408f116c90cbe3c11c389d9b6109981337df921
Phuong LeCong <phuong@lecong.org>
2018-08-03 09:14:04 -0700

    Add sample client project

---
+++ sample/client/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Hello, world!</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
